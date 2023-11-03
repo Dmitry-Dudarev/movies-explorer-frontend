4b5beed0226d26c5e0281a99b764ffa3d26fcaa7
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-11-03 22:32:50 +0300

    ref: Удален код для отладки

--- src/components/ProtectedRouteElement/ProtectedRouteElement.js
+++ src/components/ProtectedRouteElement/ProtectedRouteElement.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 function ProtectedRouteElement({ element: Component, ...props }) {
-  console.log(props.loggedIn)
   return (
     props.loggedIn ? <Component {...props} /> : <Navigate to='/' replace />
   );
